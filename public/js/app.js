e071b7953f8060005b7d20554cc6d5f3ff1350b4
parigutierrez <parivash.gutierrez@gmail.com>
2017-06-11 21:31:21 -0500

    Se agrega arreglo de imágenes de los pokemons y se modifica ruta de acceso a imágenes

--- public/js/app.js
+++ public/js/app.js
@@ -25,9 +25,10 @@ function crearSquads(squads) {
 	});
 }
 */
-alert("holaaaa");
-var xhr = new XMLHttpRequest();
 
+
+
+/*
 xhr.onreadystatechange = function (e) {
 	if (this.readyState === 4) {
 		if (this.status === 200) {
@@ -55,9 +56,76 @@ function crearPokemons(pokemons) {
 	});
 }
 
+*/
+var xhr = new XMLHttpRequest();
 
+var imagenes = [
 
+{ "imagen":"img/bulbasaur.png" },
 
+{ "imagen":"img/ivysaur.png" },
+
+{ "imagen":"img/venusaur.png" },
+
+{ "imagen":"img/charmander.png" },
+
+{ "imagen":"img/charmeleon.png" },
+
+{ "imagen":"img/charizard.png" },
+
+{ "imagen":"img/squirtle.png" },
+
+{ "imagen":"img/wartortle.png" },
+
+{ "imagen":"img/blastoise.png" },
+
+{ "imagen":"img/caterpie.png" },
+
+{ "imagen":"img/metapod.png" },
+
+{ "imagen":"img/butterfree.png" },
+
+{ "imagen":"img/weedle.png" },
+
+{ "imagen":"img/kakuna.png" },
+
+{ "imagen":"img/beedrill.png" },
+
+{ "imagen":"img/pidgey.png" },
+
+{ "imagen":"img/pidgeotto.png" },
+
+{ "imagen":"img/pidgeot.png" },
+
+{ "imagen":"img/rattata.png" },
+
+{ "imagen":"img/raticate.png" },
+
+]
+
+var plantilla = "<div class='col s3 card tarjeta hoverable'>"+ 
+					'<img src="**imagen**" class="center-align">' + 
+					'<h6 class="center-align">' +
+					'**nombrePokemon**'+ '</h6>' +
+					'</div>'
+
+$.getJSON("http://pokeapi.co/api/v2/pokemon/", function(response){
+	//console.log(response);
+	var pokemons = response.results;
+	crearPokemons(pokemons, imagenes);
+})
+
+function crearPokemons(pokemons, imagenes) {
+	var $contPokemones = $("#pokemons");
+var plantillaFinal = "";
+	pokemons.forEach(function (pokemon, indice) {
+		plantillaFinal += plantilla.replace("**nombrePokemon**", pokemon.name)
+				.replace("**imagen**", imagenes[indice].imagen);
+  });
+	$contPokemones.html(plantillaFinal);  
+	
+	
+}
 
 
 
